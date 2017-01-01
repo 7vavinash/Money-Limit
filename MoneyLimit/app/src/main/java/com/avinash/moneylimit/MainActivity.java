c437b488244df02892d8d3d55d7514c2742c14f1
7vavinash <7vavinash@gmail.com>
2017-01-01 19:33:36 +0530

    Added Google Auth with firebase

---
+++ MoneyLimit/app/src/main/java/com/avinash/moneylimit/MainActivity.java
@@ -0,0 +1,44 @@
+package com.avinash.moneylimit;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+public class MainActivity extends AppCompatActivity {
+
+    DatabaseReference mDataBase = FirebaseDatabase.getInstance().getReference();
+    DatabaseReference mData = mDataBase.child("data");
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        mData.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+}
